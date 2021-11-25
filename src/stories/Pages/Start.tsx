747c7ae6c2943af17605fcdd0e2e241c33028cce
alicespringfeld <alice.springfeld@yahoo.com>
2021-11-25 10:41:22 +0100

    :lipstick: Center backgroundimage and logo

--- src/stories/Pages/Start.tsx
+++ src/stories/Pages/Start.tsx
@@ -4,7 +4,7 @@ import styles from './Start.module.css';
 export default function Start() {
   return (
     <div className={styles.mainContainer}>
-      <img src="src/assets/Logo.png" alt="logo" />
+      <img src="src/assets/Logo.png" alt="logo" className={styles.logo} />
     </div>
   );
 }
