52a81abeb19530fcaa5433b282409b8c5c1e3a20
alicespringfeld <alice.springfeld@yahoo.com>
2021-11-26 10:57:45 +0100

    :art: Changed css import

--- src/stories/Pages/MapOverview.tsx
+++ src/stories/Pages/MapOverview.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import styles from './MapOverview.module.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-export default function MapOverview(): JSX.Element {
+export default function MapOverview() {
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={true}
-      className={styles.leafletContainer}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className={styles.leafletContainer}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
