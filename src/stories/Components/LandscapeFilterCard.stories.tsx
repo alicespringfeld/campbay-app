b06b7e68ddab0a59f67a3c1fda436621959fdd83
alicespringfeld <alice.springfeld@yahoo.com>
2021-11-29 10:47:37 +0100

    :sparkles: Add landscapefilter-card

---
+++ src/stories/Components/LandscapeFilterCard.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LandscapeFilterCard from './LandscapeFilterCard';
+
+export default {
+  component: LandscapeFilterCard,
+  title: 'Components/LandscapeFilterCard',
+};
+
+export const Default = () => <LandscapeFilterCard />;
