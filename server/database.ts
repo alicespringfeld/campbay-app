838614a67720f44308b1ca9b1f6df1f3a9113e37
alicespringfeld <alice.springfeld@yahoo.com>
2021-11-29 11:55:50 +0100

    :sparkles: Connect database

---
+++ server/database.ts
@@ -0,0 +1,8 @@
+import { MongoClient } from 'mongodb';
+
+let client: MongoClient;
+
+export async function connectDatabase(url: string) {
+  client = new MongoClient(url);
+  await client.connect();
+}
--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
-import path from 'path';
+//import path from 'path';
+import { connectDatabase } from './database';
+
+if (!process.env.MONGODB_URL) {
+  throw new Error('No MongoDB URL dotenv variable');
+}
 
 const port = process.env.PORT || 3001;
 const app = express();
+app.use(express.json());
 
 app.get('/api/hello', (_request, response) => {
   response.json({ message: 'Hello from the other side!' });
 });
 
-// Serve production bundle
-app.use(express.static('dist'));
-
 // Handle client routing, return all requests to the app
-app.get('*', (_request, response) => {
-  response.sendFile(path.join(__dirname, '../dist/index.html'));
-});
+//app.get('*', (_request, response) => {
+// response.sendFile(path.join(__dirname, '../dist/index.html'));
+//});
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+connectDatabase(process.env.MONGODB_URL).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
