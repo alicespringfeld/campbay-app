ea7a960b04af3a608fb4084ee0367684c971cdd6
alicespringfeld <alice.springfeld@yahoo.com>
2021-11-29 21:52:01 +0100

    :lipstick: finish styling of landscape-card

--- src/stories/Components/LandscapeFilterCard.tsx
+++ src/stories/Components/LandscapeFilterCard.tsx
@@ -4,6 +4,7 @@ import styles from './LandscapeFilterCard.module.css';
 export default function LandscapeFilterCard() {
   return (
     <section className={styles.mainContainer}>
+      <p className={styles.header}>Landscape</p>
       <div className={styles.container}>
         <div className={styles.tabsTextContainer}>
           <button className={styles.landscapeTabs}>
@@ -14,42 +15,45 @@ export default function LandscapeFilterCard() {
           </button>
           <p>Forrest</p>
         </div>
-      </div>
-      <div className={styles.tabsTextContainer}>
-        <button className={styles.landscapeTabs}>
-          <img
-            src="src/assets/FilterIcons_Campbay/mountain 1.svg"
-            alt="Mountains"
-          />
-        </button>
-        <p>Mountains</p>
-      </div>
-      <div className={styles.tabsTextContainer}>
-        <button className={styles.landscapeTabs}>
-          <img src="src/assets/FilterIcons_Campbay/fields 1.svg" alt="Field" />
-        </button>
-        <p>Field</p>
-      </div>
-      <div className={styles.tabsTextContainer}>
-        <button className={styles.landscapeTabs}>
-          <img src="src/assets/FilterIcons_Campbay/river 1.svg" alt="River" />
-        </button>
-        <p>River</p>
-      </div>
-      <div className={styles.tabsTextContainer}>
-        <button className={styles.landscapeTabs}>
-          <img src="src/assets/FilterIcons_Campbay/lake 1.svg" alt="Lake" />
-        </button>
-        <p>Lake</p>
-      </div>
-      <div className={styles.tabsTextContainer}>
-        <button className={styles.landscapeTabs}>
-          <img
-            src="src/assets/FilterIcons_Campbay/cityscape 1.svg"
-            alt="City"
-          />
-        </button>
-        <p>City</p>
+        <div className={styles.tabsTextContainer}>
+          <button className={styles.landscapeTabs}>
+            <img
+              src="src/assets/FilterIcons_Campbay/mountain 1.svg"
+              alt="Mountains"
+            />
+          </button>
+          <p>Mountains</p>
+        </div>
+        <div className={styles.tabsTextContainer}>
+          <button className={styles.landscapeTabs}>
+            <img
+              src="src/assets/FilterIcons_Campbay/fields 1.svg"
+              alt="Field"
+            />
+          </button>
+          <p>Field</p>
+        </div>
+        <div className={styles.tabsTextContainer}>
+          <button className={styles.landscapeTabs}>
+            <img src="src/assets/FilterIcons_Campbay/river 1.svg" alt="River" />
+          </button>
+          <p>River</p>
+        </div>
+        <div className={styles.tabsTextContainer}>
+          <button className={styles.landscapeTabs}>
+            <img src="src/assets/FilterIcons_Campbay/lake 1.svg" alt="Lake" />
+          </button>
+          <p>Lake</p>
+        </div>
+        <div className={styles.tabsTextContainer}>
+          <button className={styles.landscapeTabs}>
+            <img
+              src="src/assets/FilterIcons_Campbay/cityscape 1.svg"
+              alt="City"
+            />
+          </button>
+          <p>City</p>
+        </div>
       </div>
     </section>
   );
