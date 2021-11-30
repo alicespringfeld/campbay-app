10074885a8c5de0474bc83d51064a1d58793adf4
alicespringfeld <alice.springfeld@yahoo.com>
2021-11-30 18:52:30 +0100

    :sparkles: display locations on the map from a json-file

--- src/stories/Pages/MapOverview.tsx
+++ src/stories/Pages/MapOverview.tsx
@@ -4,15 +4,20 @@ import 'leaflet/dist/leaflet.css';
 import styles from './MapOverview.module.css';
 import SearchBar from '../Components/SearchBar';
 import FooterBar from '../Components/FooterBar';
+import sitesData from '../../data/sites.json';
 
 export default function MapOverview(): JSX.Element {
+  const filteredLocations = sitesData.filter(
+    (site) => site.address.area === 'Bavaria'
+  );
+
   return (
     <div className={styles.mapPage}>
       <SearchBar />
 
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={[51.165691, 10.451526]}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.leafletContainer}
       >
@@ -20,11 +25,20 @@ export default function MapOverview(): JSX.Element {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {filteredLocations.map((site) => (
+          <Marker
+            key={site.id}
+            position={[site.gps.latitude, site.gps.longitude]}
+          >
+            <Popup position={[site.gps.latitude, site.gps.longitude]}>
+              <div>
+                <h3>
+                  {'Adress: ' + site.address.street + ', ' + site.address.city}
+                </h3>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
       <button className={styles.navigateButton}>
         <img
