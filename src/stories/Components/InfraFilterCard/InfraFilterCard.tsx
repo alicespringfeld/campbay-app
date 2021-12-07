c30dcc7e9dc676e03e71d6c2f13ecdfdce243f87
alicespringfeld <alice.springfeld@yahoo.com>
2021-12-07 16:38:16 +0100

    :sparkles: Add Change Color onclick Infra-Tabs

--- src/stories/Components/InfraFilterCard/InfraFilterCard.tsx
+++ src/stories/Components/InfraFilterCard/InfraFilterCard.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './InfraFilterCard.module.css';
 
 export default function InfraFilterCard() {
+  const [costsSelected, setCostsSelected] = useState('var(--color-tabs)');
+  const [timeSelected, setTimeSelected] = useState('var(--color-tabs)');
+  const [toiletSelected, setToiletSelected] = useState('var(--color-tabs)');
+  const [waterSelected, setWaterSelected] = useState('var(--color-tabs)');
+  const [fireSelected, setFireSelected] = useState('var(--color-tabs)');
+  const [wasteSelected, setWasteSelected] = useState('var(--color-tabs)');
+  const [shoppingSelected, setShoppingSelected] = useState('var(--color-tabs)');
+  const [powerSelected, setPowerSelected] = useState('var(--color-tabs)');
+
   return (
     <div className={styles.mainContainer}>
       <article>
@@ -10,7 +19,15 @@ export default function InfraFilterCard() {
 
       <div className={styles.tabContainer}>
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              costsSelected === 'var(--color-tabs)'
+                ? setCostsSelected('green')
+                : setCostsSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: costsSelected }}
+            className={styles.infraTabs}
+          >
             <img
               src="src/assets/FilterIcons_Campbay/dollar-bill 1.svg"
               alt="Costs"
@@ -20,7 +37,15 @@ export default function InfraFilterCard() {
         </div>
 
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              timeSelected === 'var(--color-tabs)'
+                ? setTimeSelected('green')
+                : setTimeSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: timeSelected }}
+            className={styles.infraTabs}
+          >
             <img
               src="src/assets/FilterIcons_Campbay/stopwatch 1.svg"
               alt="timelimit"
@@ -30,7 +55,15 @@ export default function InfraFilterCard() {
         </div>
 
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              toiletSelected === 'var(--color-tabs)'
+                ? setToiletSelected('green')
+                : setToiletSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: toiletSelected }}
+            className={styles.infraTabs}
+          >
             <img
               src="src/assets/FilterIcons_Campbay/toilet 1.svg"
               alt="toilet"
@@ -40,7 +73,15 @@ export default function InfraFilterCard() {
         </div>
 
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              waterSelected === 'var(--color-tabs)'
+                ? setWaterSelected('green')
+                : setWaterSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: waterSelected }}
+            className={styles.infraTabs}
+          >
             <img
               src="src/assets/FilterIcons_Campbay/water-tap 1.svg"
               alt="tapwater"
@@ -50,7 +91,15 @@ export default function InfraFilterCard() {
         </div>
 
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              fireSelected === 'var(--color-tabs)'
+                ? setFireSelected('green')
+                : setFireSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: fireSelected }}
+            className={styles.infraTabs}
+          >
             <img
               src="src/assets/FilterIcons_Campbay/fire 1.svg"
               alt="fireplace"
@@ -60,17 +109,33 @@ export default function InfraFilterCard() {
         </div>
 
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              wasteSelected === 'var(--color-tabs)'
+                ? setWasteSelected('green')
+                : setWasteSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: wasteSelected }}
+            className={styles.infraTabs}
+          >
             <img
               src="src/assets/FilterIcons_Campbay/trash (1) 1.svg"
               alt="waste"
             />
           </button>
-          <p>Fireplace allowed</p>
+          <p>Waste disposal</p>
         </div>
 
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              shoppingSelected === 'var(--color-tabs)'
+                ? setShoppingSelected('green')
+                : setShoppingSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: shoppingSelected }}
+            className={styles.infraTabs}
+          >
             <img
               src="src/assets/FilterIcons_Campbay/shopping-cart 1.svg"
               alt="shopping"
@@ -80,7 +145,15 @@ export default function InfraFilterCard() {
         </div>
 
         <div className={styles.tabsTextContainer}>
-          <button className={styles.infraTabs}>
+          <button
+            onClick={() => {
+              powerSelected === 'var(--color-tabs)'
+                ? setPowerSelected('green')
+                : setPowerSelected('var(--color-tabs)');
+            }}
+            style={{ backgroundColor: powerSelected }}
+            className={styles.infraTabs}
+          >
             <img src="src/assets/FilterIcons_Campbay/plug 1.svg" alt="power" />
           </button>
           <p>Power access</p>
