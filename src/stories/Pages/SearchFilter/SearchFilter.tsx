0bd58f9893fd2c4ff1fa30cc819fe054d3ea9372
alicespringfeld <alice.springfeld@yahoo.com>
2021-12-07 14:18:00 +0100

    :lipstick: Add footerbar and styling

--- src/stories/Pages/SearchFilter/SearchFilter.tsx
+++ src/stories/Pages/SearchFilter/SearchFilter.tsx
@@ -6,8 +6,14 @@ import styles from '../../Pages/SearchFilter/SearchFilter.module.css';
 export default function SearchFilter() {
   return (
     <div className={styles.pageContainer}>
-      <LandscapeFilterCard />
-      <InfraFilterCard />
+      <div className={styles.cards}>
+        <LandscapeFilterCard />
+        <InfraFilterCard />
+      </div>
+      <div className={styles.footer}>
+        <img className={styles.cancel} src="src/assets/X-Icon.svg" />
+        <img className={styles.filter} src="src/assets/Filter_Button.svg" />
+      </div>
     </div>
   );
 }
