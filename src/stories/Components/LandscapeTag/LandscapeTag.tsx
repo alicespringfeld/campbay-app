275847e0502b439b0f9d8a973c9d2ae66b923f2d
alicespringfeld <alice.springfeld@yahoo.com>
2021-12-08 14:56:48 +0100

    :sparkles: Create LandscapeTag Component

---
+++ src/stories/Components/LandscapeTag/LandscapeTag.tsx
@@ -0,0 +1,32 @@
+import styles from './LandscapeTag.module.css';
+
+type LandscapeTagProps = {
+  tag: {
+    text: string;
+    src: string;
+    selected: boolean;
+    id: number;
+  };
+  onClick: (value: number) => void;
+};
+
+export default function LandscapeTag({
+  tag,
+  onClick,
+}: LandscapeTagProps): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <button
+        className={`${styles.tag} ${
+          tag.selected
+            ? styles['tag--state-selected']
+            : styles['tag--state-unselected']
+        }`}
+        onClick={() => onClick(tag.id)}
+      >
+        <img src={tag.src} />
+      </button>
+      {tag.text}
+    </div>
+  );
+}
