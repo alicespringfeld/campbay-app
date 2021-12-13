c3aad81cfa8e59d16138b34b627957f233b7b8df
alicespringfeld <alice.springfeld@yahoo.com>
2021-12-13 10:31:48 +0100

    :truck: Rename icon files

--- src/stories/Pages/SearchFilter/SearchFilter.tsx
+++ src/stories/Pages/SearchFilter/SearchFilter.tsx
@@ -6,37 +6,37 @@ import styles from './SearchFilter.module.css';
 const landscapeTags = [
   {
     text: 'Forrest',
-    src: 'src/assets/FilterIcons_Campbay/Forrest_Icon.svg',
+    src: 'src/assets/FilterIcons_Campbay/forrest.svg',
     selected: false,
     id: 0,
   },
   {
     text: 'Mountains',
-    src: 'src/assets/FilterIcons_Campbay/mountain 1.svg',
+    src: 'src/assets/FilterIcons_Campbay/mountains.svg',
     selected: false,
     id: 1,
   },
   {
     text: 'Field',
-    src: 'src/assets/FilterIcons_Campbay/fields 1.svg',
+    src: 'src/assets/FilterIcons_Campbay/fields.svg',
     selected: false,
     id: 2,
   },
   {
     text: 'River',
-    src: 'src/assets/FilterIcons_Campbay/river 1.svg',
+    src: 'src/assets/FilterIcons_Campbay/river.svg',
     selected: false,
     id: 3,
   },
   {
     text: 'Lake',
-    src: 'src/assets/FilterIcons_Campbay/lake 1.svg',
+    src: 'src/assets/FilterIcons_Campbay/lake.svg',
     selected: false,
     id: 4,
   },
   {
     text: 'City',
-    src: 'src/assets/FilterIcons_Campbay/cityscape 1.svg',
+    src: 'src/assets/FilterIcons_Campbay/city.svg',
     selected: false,
     id: 5,
   },
