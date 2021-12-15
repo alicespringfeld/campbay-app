2cf2d2af349e2a5672ea7d8c53076622394988d9
alicespringfeld <alice.springfeld@yahoo.com>
2021-12-15 23:12:03 +0100

    :fire: Removed Button Component and replaced it with link

--- src/stories/Components/Button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from './Button';
-
-export default {
-  component: Button,
-  title: 'Components/Button',
-};
-
-export const Default = () => <Button />;
--- src/stories/Pages/Start/Start.tsx
+++ src/stories/Pages/Start/Start.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import styles from './Start.module.css';
-import Button from '../../Components/Button';
 import Logo from '../../../assets/Logo.png';
-import { Link } from 'react-router-dom';
 
 export default function Start(): JSX.Element {
   return (
     <div className={styles.container}>
       <div className={styles.mainContainer}>
-        <img src={Logo} alt="logo" className={styles.logo} />
-        <Link to="/map">
-          <Button />
-        </Link>
+        <img src={Logo} alt="campBayLogo" className={styles.logo} />
+        <a href={'/map'} className={styles.startTab}>
+          Find your Bay!
+        </a>
       </div>
     </div>
   );
