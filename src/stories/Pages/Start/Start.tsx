a8261144522c05e818ac00a4f26258d2401e2678
alicespringfeld <alice.springfeld@yahoo.com>
2021-12-18 17:10:53 +0100

    :lipstick: Add responsive design to startpage

--- src/stories/Pages/Start/Start.tsx
+++ src/stories/Pages/Start/Start.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Start.module.css';
-import Logo from '../../../assets/Logo.png';
+// import Logo from '../../../assets/Logo.png';
+import LogoWithoutIcon from '../../../assets/LogoWithoutIcon.png';
+import LogoIcon from '../../../assets/CamperIcon_white.png';
 
 export default function Start(): JSX.Element {
   return (
     <div className={styles.container}>
-      <div className={styles.mainContainer}>
-        <img src={Logo} alt="campBayLogo" className={styles.logo} />
-        <a href={'/map'} className={styles.startTab}>
-          Find your Bay!
-        </a>
-      </div>
+      <section className={styles.logoContainer}>
+        <img src={LogoIcon} alt="logoIcon" className={styles.logoIcon} />
+        <span className={styles.logoText}>CampBay</span>
+      </section>
+      <a href={'/map'} className={styles.startTab}>
+        Find your Bay!
+      </a>
     </div>
   );
 }
